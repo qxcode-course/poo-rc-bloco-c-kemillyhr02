31fc40a63bdd02269e3649a5f6281275d98816fd
Kemyhr02 <kemillyholanda11@gmail.com>
2025-11-13 02:06:34 +0000

    commit

---
+++ myrep/poo/array/py/draft.py
@@ -0,0 +1,19 @@
+ary=[]
+ary= [1,2,3,4,5]
+print(ary)
+#tamanhoary
+t= len(ary)
+print(t)
+#add_remover_f
+l1=[2,4,6,3,9]
+l2= l1.append(8) 
+l3= l1.pop(4)
+print(l1)
+print(l2)
+print(l3)
+#add_rev_i
+l1=[2,6,4,3,1]
+l2= l1.insert(5,8)
+l3= l1.pop(0)
+print(l1,l3)
+#add_pop_espec
