1ab6a953675c9c47641d30721398421db06ada88
Kemyhr02 <kemillyholanda11@gmail.com>
2025-11-16 18:43:10 +0000

    3 horas

--- myrep/poo/lapiseira/py/draft.py
+++ myrep/poo/lapiseira/py/draft.py
@@ -3,24 +3,23 @@ class Grafite:
         self.__thickness: float = thickness
         self.__hardness: str = hardness
         self.__size : int = size
-    def usarPorfolha(self):
+       
+    def sagePerSheet(self):
         if self.__hardness == "HB": 
             return 1
         if self.__hardness == "2B":
             return 2
         if self.__hardness == "4B":
             return 4
-        if self.__hardness == "6B":
-            return 6 
-        elif self.__hardness == "10B":
-            return 10
+        else:
+            return 6
 
         
     def set_size(self, size: int):
         self.__size = size
     
     def __str__(self):
-        return f"[{self.__thickness}: {self.__hardness}: {self.__size}]"
+        return f"[{self.__thickness}:{self.__hardness}:{self.__size}]"
     
     def get_thickness(self):
         return self.__thickness
@@ -32,59 +31,65 @@ class Grafite:
         return self.__size
 
 class Lapiseira:
-    def __init__(self, thickness: float = 0, grafite:Grafite | None = None):
+    def __init__(self, thickness: float = 0, lead:Grafite | None = None):
         self.__thickess = thickness
-        self.__grafite = grafite 
-        self.__tambor = []
+        self.__lead= lead
+        self.__barrel =[]
 
-    def hasGrafite(self):
-        return self.__grafite != None
+    def hasGrafite(self)-> bool:
+        return self.__lead != None
 
     def insert(self, grafite:Grafite):
         if(self.__thickess != grafite.get_thickness()):
-            print("fail: calibre incompativel")
+            print("fail: calibre incompatível")
             return
         if(self.hasGrafite()):
             print("fail: ja existe grafite")
             return 
         
-        self.__tambor.append(grafite) 
+        self.__barrel.append(grafite)
+    
     def pull(self):
-        if self.__grafite is not None: 
-            print ("fail: já existe grafite no bico")
+        if self.__lead is not None: 
+            print ("fail: ja existe grafite no bico")
             return
-        if len(self.__tambor) ==0:
+        if len(self.__barrel) == 0:
             print("fail: não existe grafite no tambor")
             return
-        self.__tambor=self.__grafite[0]
+        self.__lead=self.__barrel[0]
+        self.__barrel.pop(0)
         
-        
-
-        
+     
     def remove(self):
         if not self.hasGrafite():
             print("fail: nao existe grafite")
             return 
-        self.__grafite = None
+        self.__lead = None
 
     def write(self):
         if not self.hasGrafite():
-            print("fail: nao existe grafite")
+            print("fail: nao existe grafite no bico")
             return
-        if  self.__grafite.get_size() <= 10:
+        if  self.__lead.get_size() <= 10:
             print("fail: tamanho insuficiente")
             return
-        n = self.__grafite.usarPorfolha()
+        
+        k = self.__lead.sagePerSheet()
 
-        if self.__grafite.get_size() - n <10:
+        if self.__lead.get_size() -k <10:
             print("fail: folha incompleta")
-            self.__grafite.set_size(10)
+            self.__lead.set_size(10)
             return 
-        self.__grafite.set_size(self.__grafite.get_size() - n)
+        self.__lead.set_size(self.__lead.get_size()- k)
 
     def __str__(self):
-        return f"calibre: {self.__thickess}, grafite: {"null" if self.__grafite == None else self.__grafite}"
-    
+        lead = "[]" if self.__lead is None else str(self.__lead)
+        ans = f"calibre: {self.__thickess}, bico: {lead}, tambor: <"
+        for i, p in enumerate(self.__barrel):
+            if p is not None:
+                ans+= str(p)
+        ans +=">"
+        return ans
 def main():
     lapiseira= Lapiseira()
     while True:
@@ -104,6 +109,8 @@ def main():
             lapiseira.remove()
         elif args[0]=="write":
             lapiseira.write()
+        elif args[0]=="pull":
+            lapiseira.pull()
         else: 
             print("fail: comando invalido")
     
